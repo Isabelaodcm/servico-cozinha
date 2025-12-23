32eb15aac5ba48d557cf3712e663e9189ab92f5a
Isabela <isabela.moreira8721@gmail.com>
2025-12-22 23:36:11 -0300

    commit inicial

---
+++ src/main/java/com/cozinha/servico_cozinha/ServicoCozinhaApplication.java
@@ -0,0 +1,13 @@
+package com.cozinha.servico_cozinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoCozinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoCozinhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cozinha/servico_cozinha/ServicoCozinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cozinha.servico_cozinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoCozinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
